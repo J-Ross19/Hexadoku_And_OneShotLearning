9ebdd24592e5f5175bb3a8946327e9c7899e7464
Joshua J. Ross <jjr276@ilab1.cs.rutgers.edu>
2019-10-20 18:41:56 -0400

    Add support for matrix only input in readMatrixVector, change integer arrays to double arrays, finish prediction algorithm

--- pa2/third/third.c
+++ pa2/third/third.c
@@ -14,6 +14,7 @@ int main(int argc, char** argv) {
 		return 0;
 	}
 	
+	// Part 1: Training
 	int numAttributes = 0;
 	int numExamples = 0;
 	if (fscanf(fp, "%d\n", &numAttributes) == EOF || fscanf(fp, "%d\n", &numExamples) == EOF) {
@@ -25,25 +26,54 @@ int main(int argc, char** argv) {
 	int colsX = numAttributes - 1;
 	
 	// Create matrix X with N rows and K columns
-	int** X = allocateMatrix(rowsX, colsX);
+	double** X = allocateMatrix(rowsX, colsX);
 	
 	// Create column vector Y
-	int* Y = malloc(numExamples * sizeof(int));
+	double** Y = allocateMatrix(rowsX, 1);
 	
 	// Read in value for X and Y
 	setMatrixVector(fp, X, Y, numExamples, numAttributes);
 	
-	// Calculate W = (X^T*X)^-18X^T*Y
-	int** transposeX = matrixTranspose(X, rowsX, colsX);
-	int** productXT_X = multiplyMatrix(transposeX, X, colsX, rowsX, rowsX, colsX);
-	int** inverseXT_X = calculateInverse(productXT_X, colsX);
+	// Calculate weights : W = (X^T*X)^-1*X^T*Y
+	double** transposeX = matrixTranspose(X, rowsX, colsX);
+	double** productXT_X = multiplyMatrix(transposeX, X, colsX, rowsX, rowsX, colsX);
+	double** inverseXT_X = calculateInverse(productXT_X, colsX);
 	freeMatrix(productXT_X, colsX); // Done w X^T*X
-	int** pseudoInverse = multiplyMatrix(inverseXT_X, transposeX, colsX, colsX, colsX, rowsX);
-	freeMatrix(transposeX, colsX);
-	freeMatrix(inverseXT_X, colsX);
+	double** pseudoInverse = multiplyMatrix(inverseXT_X, transposeX, colsX, colsX, colsX, rowsX);
+	freeMatrix(transposeX, colsX); // Done w X^T
+	freeMatrix(inverseXT_X, colsX); // Done w (X^T*X)^-1
+	double** W = multiplyMatrix(pseudoInverse, Y, colsX, rowsX, rowsX, 1);
+	freeMatrix(pseudoInverse, colsX); // Done w (X^T*X)^-1*X^T
+	freeMatrix(Y, rowsX); // Done w Y
+	freeMatrix(X, rowsX); // Done w X
 	
+	fclose(fp); // Close first file
 	
-	freeMatrix(X, numExamples);
+	// Part 2: Prediction
+	//MIGHT NOTT WORK
+	fp = fopen(argv[2], "r");
+	if (fp == NULL) {
+		return 0; // No file, or file cannot be accessed
+	}
+	
+	int numDataPoints = 0;
+	if (fscanf(fp, "%d\n", &numDataPoints) == EOF) {
+		// Invalid file format
+		return 0;
+	}
+	
+	X = allocateMatrix(numDataPoints, colsX);
+	setMatrixVector(fp, X, NULL, numDataPoints, colsX);
+	double** predictionCosts = multiplyMatrix(X, W, numDataPoints, colsX, colsX, 1);
+	
+	//Print Prediction Costs
+	for (int i = 0; i < numDataPoints; i++) {
+		printf("%0.0lf\n", predictionCosts[i][0]);
+	}
+	
+	freeMatrix(X, numDataPoints);
+	freeMatrix(W, colsX);
+	fclose(fp);
 	return 0;
 }
  
@@ -52,10 +82,10 @@ int main(int argc, char** argv) {
 ** Inputs: Number of rows, Number of Columns
 ** Outputs: Allocated matrix
 */
-int** allocateMatrix(int rows, int cols) {
-	int** matrix = malloc(rows * sizeof(int*));
+double** allocateMatrix(int rows, int cols) {
+	double** matrix = malloc(rows * sizeof(double*));
 	for (int i = 0; i < rows; i++) {
-		matrix[i] = malloc(cols * sizeof(int));
+		matrix[i] = malloc(cols * sizeof(double));
 	}
 	return matrix;
 }
@@ -65,19 +95,23 @@ int** allocateMatrix(int rows, int cols) {
 ** Inputs: Pointer to a file, row x col matrix, column vector with size of rows, 
 ** number of rows, number of cols
 */
-void setMatrixVector(FILE* fp, int** matrix, int* vector, int rows, int cols) {
+void setMatrixVector(FILE* fp, double** matrix, double** vector, int rows, int cols) {
 	for (int i = 0; i < rows; i++) {
-		int  num = 0;
+		double  num = 0;
 		for (int j = 0; j < cols - 1; j++) {
-			if (fscanf(fp, "%d\t", &num) == EOF) {
+			if (fscanf(fp, "%lf\t", &num) == EOF) {
 				exit(0);
 			}
 			matrix[i][j] = num;
 		}
-		if (fscanf(fp, "%d\n", &num) == EOF) {
-			exit(0);
+		if (fscanf(fp, "%lf\n", &num) == EOF) {
+			exit(0); 
 		}
-		vector[i] = num;
+		if (vector != NULL) {
+			vector[i][0] = num;
+		} else {
+			matrix[i][cols - 1] = num;
+		}
 	}
 }
 
@@ -86,8 +120,8 @@ void setMatrixVector(FILE* fp, int** matrix, int* vector, int rows, int cols) {
 ** Inputs: Matrix, number of rows, number of cols
 ** Outputs: The transposed matrix of the input
 */
-int** matrixTranspose(int** matrix, int rows, int cols) {
-	int** transpose = allocateMatrix(cols, rows);
+double** matrixTranspose(double** matrix, int rows, int cols) {
+	double** transpose = allocateMatrix(cols, rows);
 	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j < cols; j++) {
 			transpose[j][i] = matrix[i][j];
@@ -99,14 +133,14 @@ int** matrixTranspose(int** matrix, int rows, int cols) {
 /*
 ** Function to multiply two matrices together
 */
-int** multiplyMatrix(int** m1, int** m2, int row1, int col1, int row2, int col2) {
+double** multiplyMatrix(double** m1, double** m2, int row1, int col1, int row2, int col2) {
 	
 	if (col1 != row2) {
 		// Should never happen in this case
 		return NULL;
 	}
 	
-	int** product = allocateMatrix(row1, col2);
+	double** product = allocateMatrix(row1, col2);
 	
 	for (int i = 0; i < row1; i++) {
 		for (int j = 0; j < col2; j++) {
@@ -124,8 +158,8 @@ int** multiplyMatrix(int** m1, int** m2, int row1, int col1, int row2, int col2)
 /*
 ** Function to calculate inverse of a matrix
 */
-int** calculateInverse(int** matrix, int n) {
-	int** inverse = allocateMatrix(n, n);
+double** calculateInverse(double** matrix, int n) {
+	double** inverse = allocateMatrix(n, n);
 	// Make inverse the identity matrix
 	for (int i= 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
@@ -167,7 +201,7 @@ int** calculateInverse(int** matrix, int n) {
 /*
 ** Function to free memory of a matrix
 */
-void freeMatrix(int** matrix, int rows) {
+void freeMatrix(double** matrix, int rows) {
 	for (int i = 0; i < rows; i++) {
 		free(matrix[i]);
 	}
--- pa2/third/third.h
+++ pa2/third/third.h
@@ -1,11 +1,11 @@
 #ifndef ONE_SHOT_LEARNING_H
 #define ONE_SHOT_LEARNING_H
 
-int** allocateMatrix(int, int);
-void setMatrixVector(FILE*, int**, int*, int, int);
-int** matrixTranspose(int**, int, int);
-int** multiplyMatrix(int**, int**, int, int, int, int);
-int** calculateInverse(int**, int);
-void freeMatrix(int**, int);
+double** allocateMatrix(int, int);
+void setMatrixVector(FILE*, double**, double**, int, int);
+double** matrixTranspose(double**, int, int);
+double** multiplyMatrix(double**, double**, int, int, int, int);
+double** calculateInverse(double**, int);
+void freeMatrix(double**, int);
 
 #endif
